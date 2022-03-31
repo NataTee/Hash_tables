8c2c63ee10376d68f52557cccc13b89072a33bd2
Natalia Ksenz <velvet@MacBook-Pro.local>
2022-03-31 00:17:47 -0700

    comment header addded

--- hashtable.h
+++ hashtable.h
@@ -1,3 +1,8 @@
-/* 
- * your comment header here
- */
+/***************************************************************************
+Name: Natalia Ksenz
+Coding 06
+Purpose: The assignment to create a Hash Table class/object with standard
+structure.
+
+File: main.h
+***************************************************************************/
