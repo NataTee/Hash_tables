7016c39723de7f2365e78b4de5e46e26a5762f70
Natalia Ksenz <velvet@MacBook-Pro.local>
2022-04-02 01:34:04 -0700

    added class Hashtable

--- hashtable.h
+++ hashtable.h
@@ -4,5 +4,38 @@ Coding 06
 Purpose: The assignment to create a Hash Table class/object with standard
 structure.
 
-File: main.h
+File: hashtable.h
 ***************************************************************************/
+
+#ifndef HASHTABLE_HASHTABLE_H
+#define HASHTABLE_HASHTABLE_H
+
+#include <iostream>
+#include "data.h"
+
+#define HASHTABLESIZE 15
+
+using std::cout;
+using std::endl;
+
+class Hashtable {
+
+public:
+    Hashtable();
+    ~Hashtable();
+
+    bool insertEntry(int, string);
+    string getData(int);
+    bool removeEntry(int);
+    int getCount();
+    void printTable();
+
+    private:
+
+    int hash(int);
+
+    Data *hashtable[HASHTABLESIZE];
+    int count;
+};
+
+#endif /* HASHTABLE_HASHTABLE_H */
